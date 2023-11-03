3265b49d489219094b4d9fec0e72d7594e63028f
MiguelJimV <100383194@alumnos.uc3m.es>
2023-11-03 12:56:07 +0100

    Format block.hpp

--- sim/block.hpp
+++ sim/block.hpp
@@ -3,43 +3,44 @@
 #ifndef BLOCK
 #define BLOCK
 
-#include <vector>
-
 #include "common.hpp"
+
+#include <vector>
 
 using namespace std;
 
 int block();
 
 class Block {
-  // Representa un bloque en la malla. Realizará todos los cálculos entre
-  // partículas
- public:
-  vector<Particle> particles;
-  vector<std::pair<Particle, Particle>> particlePairs;
-  DataCommon data;
-  vector<double> acelerationX;
-  vector<double> acelerationY;
-  vector<double> acelerationZ;
-  vector<double> density;
+    // Representa un bloque en la malla. Realizará todos los cálculos entre
+    // partículas
 
-  void addParticle(Particle &particle);
-  void initVectors();
-  void densityIncrease(Block& contiguousBlock);
-  void accelerationTransfer(Block& contiguousBlock);
-  void collisionsX(unsigned int cx);
-  void collisionsY(unsigned int cy);
-  void collisionsZ(unsigned int cz);
-  void particleMotion();
-  void interactionsX(unsigned int cx);
-  void interactionsY(unsigned int cy);
-  void interactionsZ(unsigned int cz);
-  void generarParejasBloque();
-  vector<std::pair<Particle, Particle>> generarParejasEntreBloques(Block& otherBlock);
-  void calculateDataCommon();
-  void initDensityAcceleration();
-  void lineal_transformate_density();
-  double pow(double base, int exponent);
+  public:
+    vector<Particle> particles;
+    vector<std::pair<Particle, Particle>> particlePairs;
+    DataCommon data;
+    vector<double> acelerationX;
+    vector<double> acelerationY;
+    vector<double> acelerationZ;
+    vector<double> density;
+
+    void addParticle(Particle & particle);
+    void initVectors();
+    void densityIncrease(Block & contiguousBlock);
+    void accelerationTransfer(Block & contiguousBlock);
+    void collisionsX(unsigned int cx);
+    void collisionsY(unsigned int cy);
+    void collisionsZ(unsigned int cz);
+    void particleMotion();
+    void interactionsX(unsigned int cx);
+    void interactionsY(unsigned int cy);
+    void interactionsZ(unsigned int cz);
+    void generarParejasBloque();
+    vector<std::pair<Particle, Particle>> generarParejasEntreBloques(Block & otherBlock);
+    void calculateDataCommon();
+    void initDensityAcceleration();
+    void lineal_transformate_density();
+    double pow(double base, int exponent);
 };
 
 #endif
