9892af02d06bb220aadba325e40c3a2edd6c70b7
100451296 <98945796+100451296@users.noreply.github.com>
2023-11-05 14:24:32 +0100

    correcion final lectura particulas y refactorizacion en fluid

--- fluid/fluid.cpp
+++ fluid/fluid.cpp
@@ -11,7 +11,10 @@ using namespace std;
 
 int main(int argc, char * argv[]) {
   // Comprueba parametros
-  if (argc != 4) { return 1; }
+  if (argc != 4) {
+    cout << "Error: Invalid number of arguments:" << argc << endl;
+    return -1;
+  }
   if (int resultado = proargs_validations(argc, argv[1], argv[2], argv[3]) != 0) {
     return resultado;
   }
@@ -25,46 +28,7 @@ int main(int argc, char * argv[]) {
   vector<Particle> particles;
 
   readFile(input_file_name, ppm, num_particles, particles);
-
-  // Calcula parametros
-  double h             = MULTIPLICADOR_RADIO / ppm;
-  double particle_mass = DENSIDAD_FLUIDO * std::pow(ppm, -3);
-  int nx               = std::floor((LIMITE_SUPERIOR_RECINTO_X - LIMITE_INFERIOR_RECINTO_X) / h);
-  int ny               = std::floor((LIMITE_SUPERIOR_RECINTO_Y - LIMITE_INFERIOR_RECINTO_Y) / h);
-  int nz               = std::floor((LIMITE_SUPERIOR_RECINTO_Z - LIMITE_INFERIOR_RECINTO_Z) / h);
-  int num_blocks       = nx * ny * nz;
-  double sx            = (LIMITE_SUPERIOR_RECINTO_X - LIMITE_INFERIOR_RECINTO_X) / nx;
-  double sy            = (LIMITE_SUPERIOR_RECINTO_Y - LIMITE_INFERIOR_RECINTO_Y) / ny;
-  double sz            = (LIMITE_SUPERIOR_RECINTO_Z - LIMITE_INFERIOR_RECINTO_Z) / nz;
-
-  // Imprime parametros
-  std::cout << "Number of particles: " << particles.size() << std::endl;
-  std::cout << "Particles per meter: " << static_cast<int>(ppm) << std::endl;
-  std::cout << "Smoothing length: " << h << std::endl;
-  std::cout << "Particle mass: " << particle_mass << std::endl;
-  std::cout << "Grid size: " << nx << " x " << ny << " x " << nz << std::endl;
-  std::cout << "Number of blocks: " << num_blocks << std::endl;
-  std::cout << "Block size: " << sx << " x " << sy << " x " << sz << std::endl;
-
-  Grid grid(nx, ny, nz);
-
-  // Valida archivo de salida
-  std::ofstream output_file(output_file_name, std::ios::binary);
-  if (!output_file.is_open()) {
-    std::cerr << "Error: no se pudo abrir el archivo de salida." << std::endl;
-    return 1;
-  }
-
-  // Escribe la cabecera (ppm y numero de particulas)
-  output_file.write(reinterpret_cast<char *>(&ppm), sizeof(float));
-  output_file.write(reinterpret_cast<char *>(&num_particles), sizeof(int));
-
-  // Escribe particulas
-  for (int i = 0; i < num_particles; ++i) {
-    output_file.write(reinterpret_cast<char *>(&particles[i]), sizeof(Particle));
-  }
-
-  output_file.close();
+  printParameters(ppm, num_particles);
 
   return 0;
 }
