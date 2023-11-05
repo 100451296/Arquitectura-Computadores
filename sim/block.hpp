9a67a45cb7f6a65f0a2dd11e7edb94b4507b55cd
MiguelJimV <100383194@alumnos.uc3m.es>
2023-11-05 17:20:17 +0100

    Actualizacion

--- sim/block.hpp
+++ sim/block.hpp
@@ -1,5 +1,3 @@
-// Block Class
-
 #ifndef BLOCK
 #define BLOCK
 
@@ -7,8 +5,6 @@
 
 #include <vector>
 #include <memory>
-
-using namespace std;
 
 int block();
 
@@ -18,34 +14,36 @@ class Block {
 
   public:
     std::vector<std::shared_ptr<Particle>> particles;  
-    vector<std::pair< std::shared_ptr<Particle>, std::shared_ptr<Particle>>> particlePairs;
+    std::vector<std::pair<std::shared_ptr<Particle>, std::shared_ptr<Particle>>> particlePairs;
     DataCommon& data;
-    vector<double> accelerationX;
-    vector<double> accelerationY;
-    vector<double> accelerationZ;
-    vector<double> density;
+    std::vector<double> accelerationX;
+    std::vector<double> accelerationY;
+    std::vector<double> accelerationZ;
+    std::vector<double> density;
 
-  void addParticle(shared_ptr<Particle> particle);
-  void initVectors();
-  void densityIncrease(Block& contiguousBlock);
-  void accelerationTransfer(Block& contiguousBlock);
-  void collisionsX(unsigned int cx);
-  void collisionsY(unsigned int cy);
-  void collisionsZ(unsigned int cz);
-  void particleMotion();
-  void interactionsX(unsigned int cx);
-  void interactionsY(unsigned int cy);
-  void interactionsZ(unsigned int cz);
-  void generarParejasBloque();
-  vector<std::pair<std::shared_ptr<Particle>, std::shared_ptr<Particle>>> generarParejasEntreBloques(Block& otherBlock);
-  void calculateDataCommon();
-  void initDensityAcceleration();
-  void calculate_increm_density(vector<std::pair<std::shared_ptr<Particle>, std::shared_ptr<Particle>>> ParejaParticulas);
-  void lineal_transformate_density();
-  vector<double> calculate_increm_aceleration(vector<double> position, vector <double> velocity, double dist, vector<unsigned int> Id);
-  double calculate_dist(double posX, double posY, double posZ);
-  double max(double n1, double n2);
-  void accelerationTransferCalculations(vector<std::pair<std::shared_ptr<Particle>, std::shared_ptr<Particle>>> pair_vec);
+    Block(DataCommon& data) : data(data) {}
+
+    void addParticle(std::shared_ptr<Particle> particle);
+    void initVectors();
+    void densityIncrease(Block& contiguousBlock);
+    void accelerationTransfer(Block& contiguousBlock);
+    void collisionsX(unsigned int cx);
+    void collisionsY(unsigned int cy);
+    void collisionsZ(unsigned int cz);
+    void particleMotion();
+    void interactionsX(unsigned int cx);
+    void interactionsY(unsigned int cy);
+    void interactionsZ(unsigned int cz);
+    void generarParejasBloque();
+    std::vector<std::pair<std::shared_ptr<Particle>, std::shared_ptr<Particle>>> generarParejasEntreBloques(Block& otherBlock);
+    void calculateDataCommon();
+    void initDensityAcceleration();
+    void calculate_increm_density(std::vector<std::pair<std::shared_ptr<Particle>, std::shared_ptr<Particle>>> ParejaParticulas);
+    void lineal_transformate_density();
+    std::vector<double> calculate_increm_aceleration(std::vector<double> position, std::vector<double> velocity, double dist, std::vector<unsigned int> Id);
+    double calculate_dist(double posX, double posY, double posZ);
+    double max(double n1, double n2);
+    void accelerationTransferCalculations(std::vector<std::pair<std::shared_ptr<Particle>, std::shared_ptr<Particle>>> pair_vec);
 };
 
 #endif
