3a889eeb1bb67b94ccca027a65fe274392c7adb0
100383263 <100383263@alumnos.uc3m.es>
2023-11-05 15:42:20 +0100

    finiquitao

--- sim/block.cpp
+++ sim/block.cpp
@@ -8,7 +8,7 @@ using namespace std;
 
 // METODOS de BLOCK
 
-// Crear parejas de un mismo bloque
+// Metodo encargado de crear las parejas de particulas de un mismo bloque
 void Block::generarParejasBloque() {
   for (size_t i = 0; i < particles.size(); ++i) {
     for (size_t j = i + 1; j < particles.size(); ++j) {
@@ -17,6 +17,7 @@ void Block::generarParejasBloque() {
   }
 }
 
+// Metodo encargado de crear las parejas de particulas entre bloques contiguos
 vector<std::pair<ParticleRef const, ParticleRef const>>
     Block::generarParejasEntreBloques(Block & otherBlock) {
   // Generar pares entre el bloque actual y el bloque contiguo
@@ -27,7 +28,7 @@ vector<std::pair<ParticleRef const, ParticleRef const>>
     }
   }
   return aux;
-};
+}
 
 // Función para añadir una partícula al vector
 void Block::addParticle(ParticleRef particle) {
@@ -55,9 +56,9 @@ void Block::initDensityAcceleration() {
   }
 }
 
+// Funcion encargada de modificar el vector de densidades del propio bloque y transformacion lineal
 void Block::densityIncrease(Block & contiguousBlock) {
-  // Funcion encargada de modificar el vector de densidades del propio bloque y transformacion
-  // lineal Parte del bloque actual
+  // Parte del bloque actual
   calculate_increm_density(this->particlePairs);
   // Parte del bloque contiguo
   vector<std::pair<ParticleRef const, ParticleRef const>> aux =
@@ -66,6 +67,7 @@ void Block::densityIncrease(Block & contiguousBlock) {
   lineal_transformate_density();
 }
 
+// Metodo auxiliar que realiza los diferentes calculos para el incremento de densidad
 void Block::calculate_increm_density(
     vector<std::pair<ParticleRef const, ParticleRef const>> ParejaParticulas) {
   double aux_x, aux_y, aux_z, increm_density_pair;
@@ -83,6 +85,7 @@ void Block::calculate_increm_density(
   }
 }
 
+// Metodo auxiliar que realiza los diferentes calculos para la transformacion lineal de la densidad
 void Block::lineal_transformate_density() {
   for (size_t i = 0; i < density.size(); i++) {
     if (density[i] != 0) {
@@ -92,6 +95,7 @@ void Block::lineal_transformate_density() {
   }
 }
 
+// Metodo auxiliar que realiza los diferentes calculos para el incremento de aceleracion
 vector<double> Block::calculate_increm_aceleration(vector<double> position, vector<double> velocity,
                                                    double dist, vector<unsigned int> Id) {
   vector<double> increm_aceleration;
@@ -114,12 +118,13 @@ vector<double> Block::calculate_increm_aceleration(vector<double> position, vect
   return increm_aceleration;
 }
 
+// Metodo auxiliar que realiza los diferentes calculos para la distancia
 double Block::calculate_dist(double posX, double posY, double posZ) {
-  double dist;
-  dist = sqrt(max(pow(posX, 2) + pow(posY, 2) + pow(posZ, 2), pow(10, -12)));
+  double dist = sqrt(max(pow(posX, 2) + pow(posY, 2) + pow(posZ, 2), pow(10, -12)));
   return dist;
 }
 
+// Metodo auxiliar que realiza los diferentes calculos para la aceleracion
 void Block::accelerationTransferCalculations(
     vector<std::pair<ParticleRef const, ParticleRef const>> pair_vec) {
   for (auto const & pair : pair_vec) {
@@ -149,8 +154,8 @@ void Block::accelerationTransferCalculations(
   }
 }
 
+// Funcion que se encarga de actualizar el vector de aceleraciones y el incremento
 void Block::accelerationTransfer(Block & contiguousBlock) {
-  // Funcion que se encarga de actualizar el vector de aceleraciones y el incremento
   // Parte del bloque actual
   accelerationTransferCalculations(particlePairs);
   // Parte del bloque contiguo
@@ -159,32 +164,74 @@ void Block::accelerationTransfer(Block & contiguousBlock) {
   accelerationTransferCalculations(aux);
 }
 
+// Debe actualizar la componente x del vector de aceleración para cada particula
 void Block::collisionsX(unsigned int cx) {
-  // Debe actualizar la componente x del vector de aceleración para cada
-  // particula del bloque
-
-  cout << "Colisiones en la dirección X con valor de cx: " << cx << endl;
-  // Tu código aquí
+  double cord_x, increm_x = 0;
+  for (size_t i = 0; i < particles.size(); ++i) {
+    cord_x = particles[i].p1.posX + particles[i].p1.smoothVecX * PASO_TIEMPO;
+    if (cx == 0) {
+      increm_x = TAMANO_PARTICULA - (cord_x - LIMITE_INFERIOR_RECINTO_X);
+    } else if (cx == data.nx - 1) {
+      increm_x = TAMANO_PARTICULA - (LIMITE_SUPERIOR_RECINTO_X - cord_x);
+    }
+    if (increm_x > pow(10, -10)) {
+      if (cx == 0) {
+        accelerationX[i] = accelerationX[i] + COLISIONES_RIGIDEZ * increm_x -
+                           AMORTIGUAMIENTO * particles[i].p1.velX;
+      } else if (cx == data.nx - 1) {
+        accelerationX[i] = accelerationX[i] - COLISIONES_RIGIDEZ * increm_x +
+                           AMORTIGUAMIENTO * particles[i].p1.velX;
+      }
+    }
+  }
 }
 
+// Debe actualizar la componente y del vector de aceleración para cada particula
 void Block::collisionsY(unsigned int cy) {
-  // Debe actualizar la componente y del vector de aceleración para cada
-  // particula del bloque
-
-  cout << "Colisiones en la dirección Y con valor de cy: " << cy << endl;
-  // Tu código aquí
+  double cord_y, increm_y = 0;
+  for (size_t i = 0; i < particles.size(); ++i) {
+    cord_y = particles[i].p1.posY + particles[i].p1.smoothVecY * PASO_TIEMPO;
+    if (cy == 0) {
+      increm_y = TAMANO_PARTICULA - (cord_y - LIMITE_INFERIOR_RECINTO_Y);
+    } else if (cy == data.ny - 1) {
+      increm_y = TAMANO_PARTICULA - (LIMITE_SUPERIOR_RECINTO_Y - cord_y);
+    }
+    if (increm_y > pow(10, -10)) {
+      if (cy == 0) {
+        accelerationY[i] = accelerationY[i] + COLISIONES_RIGIDEZ * increm_y -
+                           AMORTIGUAMIENTO * particles[i].p1.velY;
+      } else if (cy == data.ny - 1) {
+        accelerationY[i] = accelerationY[i] - COLISIONES_RIGIDEZ * increm_y +
+                           AMORTIGUAMIENTO * particles[i].p1.velY;
+      }
+    }
+  }
 }
 
-void Block::collisionsZ(unsigned int cx) {
-  // Debe actualizar la componente z del vector de aceleración para cada
-  // particula del bloque
-
-  cout << "Colisiones en la dirección Z con valor de cx: " << cx << endl;
-  // Tu código aquí
+// Debe actualizar la componente z del vector de aceleración para cada particula
+void Block::collisionsZ(unsigned int cz) {
+  double cord_z, increm_z = 0;
+  for (size_t i = 0; i < particles.size(); ++i) {
+    cord_z = particles[i].p1.posZ + particles[i].p1.smoothVecZ * PASO_TIEMPO;
+    if (cz == 0) {
+      increm_z = TAMANO_PARTICULA - (cord_z - LIMITE_INFERIOR_RECINTO_Z);
+    } else if (cz == data.nz - 1) {
+      increm_z = TAMANO_PARTICULA - (LIMITE_SUPERIOR_RECINTO_Z - cord_z);
+    }
+    if (increm_z > pow(10, -10)) {
+      if (cz == 0) {
+        accelerationZ[i] = accelerationZ[i] + COLISIONES_RIGIDEZ * increm_z -
+                           AMORTIGUAMIENTO * particles[i].p1.velZ;
+      } else if (cz == data.nz - 1) {
+        accelerationZ[i] = accelerationZ[i] - COLISIONES_RIGIDEZ * increm_z +
+                           AMORTIGUAMIENTO * particles[i].p1.velZ;
+      }
+    }
+  }
 }
 
+// Debe actualizar el movimiento de cada particula
 void Block::particleMotion() {
-  // Debe actualizar la posicion, velocidad y vector de suavizado de cada
   for (size_t i = 0; i < particles.size(); i++) {
     particles[i].p1.posX = particles[i].p1.posX + particles[i].p1.smoothVecX * PASO_TIEMPO +
                            accelerationX[i] * pow(PASO_TIEMPO, 2);
@@ -201,30 +248,70 @@ void Block::particleMotion() {
   }
 }
 
+// Debe actualizar la posicion, velocidad y vector de suavizado de cada particula eje x
 void Block::interactionsX(unsigned int cx) {
-  // Debe actualizar la posicion, velocidad y vector de suavizado de cada
-  // particula, cuando dx sea negativo
-
-  cout << "Interacciones en la dirección X con valor de cx: " << cx << endl;
-  // Tu código aquí
+  double dx = 0;
+  for (size_t i = 0; i < particles.size(); ++i) {
+    if (cx == 0) {
+      dx = particles[i].p1.posX - LIMITE_INFERIOR_RECINTO_X;
+    } else if (cx == data.nx - 1) {
+      dx = LIMITE_SUPERIOR_RECINTO_X - particles[i].p1.posX;
+    }
+    if (dx < 0) {
+      if (cx == 0) {
+        particles[i].p1.posX = LIMITE_INFERIOR_RECINTO_X - dx;
+      } else if (cx == data.nx - 1) {
+        particles[i].p1.posX = LIMITE_SUPERIOR_RECINTO_X + dx;
+      }
+      particles[i].p1.velX       = particles[i].p1.velX * -1;
+      particles[i].p1.smoothVecX = particles[i].p1.smoothVecX * -1;
+    }
+  }
 }
 
+// Debe actualizar la posicion, velocidad y vector de suavizado de cada particula eje y
 void Block::interactionsY(unsigned int cy) {
-  // Debe actualizar la posicion, velocidad y vector de suavizado de cada
-  // particula, cuando dy sea negativo
-
-  cout << "Interacciones en la dirección Y con valor de cy: " << cy << endl;
-  // Tu código aquí
+  double dy = 0;
+  for (size_t i = 0; i < particles.size(); ++i) {
+    if (cy == 0) {
+      dy = particles[i].p1.posY - LIMITE_INFERIOR_RECINTO_Y;
+    } else if (cy == data.ny - 1) {
+      dy = LIMITE_SUPERIOR_RECINTO_Y - particles[i].p1.posY;
+    }
+    if (dy < 0) {
+      if (cy == 0) {
+        particles[i].p1.posY = LIMITE_INFERIOR_RECINTO_Y - dy;
+      } else if (cy == data.ny - 1) {
+        particles[i].p1.posY = LIMITE_SUPERIOR_RECINTO_Y + dy;
+      }
+      particles[i].p1.velY       = particles[i].p1.velY * -1;
+      particles[i].p1.smoothVecY = particles[i].p1.smoothVecY * -1;
+    }
+  }
 }
 
+// Debe actualizar la posicion, velocidad y vector de suavizado de cada particula eje z
 void Block::interactionsZ(unsigned int cz) {
-  // Debe actualizar la posicion, velocidad y vector de suavizado de cada
-  // particula, cuando dz sea negativo
-
-  cout << "Interacciones en la dirección Z con valor de cz: " << cz << endl;
-  // Tu código aquí
+  double dz = 0;
+  for (size_t i = 0; i < particles.size(); ++i) {
+    if (cz == 0) {
+      dz = particles[i].p1.posZ - LIMITE_INFERIOR_RECINTO_Z;
+    } else if (cz == data.nz - 1) {
+      dz = LIMITE_SUPERIOR_RECINTO_Z - particles[i].p1.posZ;
+    }
+    if (dz < 0) {
+      if (cz == 0) {
+        particles[i].p1.posZ = LIMITE_INFERIOR_RECINTO_Z - dz;
+      } else if (cz == data.nz - 1) {
+        particles[i].p1.posZ = LIMITE_SUPERIOR_RECINTO_Z + dz;
+      }
+      particles[i].p1.velZ       = particles[i].p1.velZ * -1;
+      particles[i].p1.smoothVecZ = particles[i].p1.smoothVecZ * -1;
+    }
+  }
 }
 
+// Metodo que devuelve el valor maximo
 double Block::max(double n1, double n2) {
   if (n1 > n2) {
     return n1;
