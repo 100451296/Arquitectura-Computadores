28e53a93d8eef4c3ff3599b2101ab155edefbfd5
MiguelJimV <100383194@alumnos.uc3m.es>
2023-11-07 19:28:29 +0100

    que no

--- sim/block.cpp
+++ sim/block.cpp
@@ -108,40 +108,15 @@ void Block::lineal_transformate_density() {
 // respecto a un bloque contiguo
 void Block::lineal_transformate_density(Block & contiguousBlock) {
   for (size_t i = 0; i < contiguousBlock.density.size(); i++) {
+    if (density[i] != 0) {
+      density[i] = (density[i] + pow(data->long_suavizado, 6)) * 315 * data->mass /
+                   (64 * numbers::pi * pow(data->long_suavizado, 9));
+    }
     if (contiguousBlock.density[i] != 0) {
       contiguousBlock.density[i] = (density[i] + pow(data->long_suavizado, 6)) * 315 * data->mass /
                                    (64 * numbers::pi * pow(data->long_suavizado, 9));
     }
   }
-}
-
-// Metodo auxiliar que realiza los diferentes calculos para el incremento de aceleracion
-vector<double> Block::calculate_increm_aceleration(vector<double> position, vector<double> velocity,
-                                                   double dist, vector<unsigned int> Id) {
-  vector<double> increm_aceleration;
-  increm_aceleration = {
-    ((position[0]) * (15 / numbers::pi * pow(data->long_suavizado, 6)) *
-         (3 * data->mass * PRESION_RIGIDEZ / 2) * ((pow(data->long_suavizado - dist, 2)) / dist) *
-         (density[Id[0]] + density[Id[1]] - 2 * DENSIDAD_FLUIDO) +
-     (velocity[0]) * (45 / numbers::pi * pow(data->long_suavizado, 6)) * VISCOSIDAD * data->mass) /
-        (density[Id[0]] * density[Id[1]]),
-    ((position[1]) * (15 / numbers::pi * pow(data->long_suavizado, 6)) *
-         (3 * data->mass * PRESION_RIGIDEZ / 2) * ((pow(data->long_suavizado - dist, 2)) / dist) *
-         (density[Id[0]] + density[Id[1]] - 2 * DENSIDAD_FLUIDO) +
-     (velocity[1]) * (45 / numbers::pi * pow(data->long_suavizado, 6)) * VISCOSIDAD * data->mass) /
-        (density[Id[0]] * density[Id[1]]),
-    ((position[2]) * (15 / numbers::pi * pow(data->long_suavizado, 6)) *
-         (3 * data->mass * PRESION_RIGIDEZ / 2) * ((pow(data->long_suavizado - dist, 2)) / dist) *
-         (density[Id[0]] + density[Id[1]] - 2 * DENSIDAD_FLUIDO) +
-     (velocity[2]) * (45 / numbers::pi * pow(data->long_suavizado, 6)) * VISCOSIDAD * data->mass) /
-        (density[Id[0]] * density[Id[1]])};
-  return increm_aceleration;
-}
-
-// Metodo auxiliar que realiza los diferentes calculos para la distancia
-double Block::calculate_dist(double posX, double posY, double posZ) {
-  double dist = sqrt(max(pow(posX, 2) + pow(posY, 2) + pow(posZ, 2), pow(10, -12)));
-  return dist;
 }
 
 // Metodo auxiliar que realiza los diferentes calculos para la aceleracion
@@ -176,7 +151,34 @@ void Block::accelerationTransferCalculations(
     }
   }
 }
+// Metodo auxiliar que realiza los diferentes calculos para el incremento de aceleracion
+vector<double> Block::calculate_increm_aceleration(vector<double> position, vector<double> velocity,
+                                                   double dist, vector<unsigned int> Id) {
+  vector<double> increm_aceleration;
+  increm_aceleration = {
+    ((position[0]) * (15 / numbers::pi * pow(data->long_suavizado, 6)) *
+         (3 * data->mass * PRESION_RIGIDEZ / 2) * ((pow(data->long_suavizado - dist, 2)) / dist) *
+         (density[Id[0]] + density[Id[1]] - 2 * DENSIDAD_FLUIDO) +
+     (velocity[0]) * (45 / numbers::pi * pow(data->long_suavizado, 6)) * VISCOSIDAD * data->mass) /
+        (density[Id[0]] * density[Id[1]]),
+    ((position[1]) * (15 / numbers::pi * pow(data->long_suavizado, 6)) *
+         (3 * data->mass * PRESION_RIGIDEZ / 2) * ((pow(data->long_suavizado - dist, 2)) / dist) *
+         (density[Id[0]] + density[Id[1]] - 2 * DENSIDAD_FLUIDO) +
+     (velocity[1]) * (45 / numbers::pi * pow(data->long_suavizado, 6)) * VISCOSIDAD * data->mass) /
+        (density[Id[0]] * density[Id[1]]),
+    ((position[2]) * (15 / numbers::pi * pow(data->long_suavizado, 6)) *
+         (3 * data->mass * PRESION_RIGIDEZ / 2) * ((pow(data->long_suavizado - dist, 2)) / dist) *
+         (density[Id[0]] + density[Id[1]] - 2 * DENSIDAD_FLUIDO) +
+     (velocity[2]) * (45 / numbers::pi * pow(data->long_suavizado, 6)) * VISCOSIDAD * data->mass) /
+        (density[Id[0]] * density[Id[1]])};
+  return increm_aceleration;
+}
 
+// Metodo auxiliar que realiza los diferentes calculos para la distancia
+double Block::calculate_dist(double posX, double posY, double posZ) {
+  double dist = sqrt(max(pow(posX, 2) + pow(posY, 2) + pow(posZ, 2), pow(10, -12)));
+  return dist;
+}
 void Block::increm_aceleration_sum(std::vector<std::shared_ptr<double>> Acceleration,
                                    std::vector<double> increm_aceleration) {
   *Acceleration[0] = *Acceleration[0] + increm_aceleration[0];
