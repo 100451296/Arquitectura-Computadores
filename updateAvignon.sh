84f8d24afcb734ffc2b1dfdaceadc251c821ec6f
100451296 <98945796+100451296@users.noreply.github.com>
2023-11-21 16:30:51 +0100

    script para subir proyecto a avignon

---
+++ updateAvignon.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# Archivos y carpetas a copiar
+archivos_a_copiar="files fluid ftest scripts sim utest .clang-tidy CMakeLists.txt"
+
+# Ruta del destino en el servidor remoto
+destino_remoto="avignon:~/proyecto/"
+
+# Copiar archivos y carpetas usando scp
+scp -r $archivos_a_copiar $destino_remoto
