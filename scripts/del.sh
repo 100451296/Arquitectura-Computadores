b3f243e25ff973b83ae9af25906c07b0d8597dbf
100451296 <98945796+100451296@users.noreply.github.com>
2023-11-21 16:35:58 +0100

    script para resetear proyecto

---
+++ scripts/del.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+
+find ~/proyecto -mindepth 1 ! -name 'CMakeLists.txt' -exec rm -r {} \;
--- updateAvignon.sh
+++ updateAvignon.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
 # Archivos y carpetas a copiar
-archivos_a_copiar="files fluid ftest scripts sim utest .clang-tidy CMakeLists.txt"
+archivos_a_copiar="files fluid ftest scripts sim utest .clang-tidy"
 
 # Ruta del destino en el servidor remoto
 destino_remoto="avignon:~/proyecto/"
