b0dbb48a0fd97b213d71238f4e3b573fd2f5e197
100451189 <100451189@alumnos.uc3m.es>
2023-11-23 17:17:53 +0100

    inclusion de sh para mediciones de rendimiento y energia

---
+++ scripts/perf-energia-1000.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+. /etc/profile
+module avail
+module load gcc/12.1.0
+
+# Configura eventos específicos para medir energía y potencia
+ENERGY_EVENTS="-e power/energy-cores/,power/energy-gpu/,power/energy-pkg/,power/energy-ram/"
+
+# Utiliza perf stat para medir la energía
+perf stat $ENERGY_EVENTS -r 5 ../build/fluid/fluid 1000 ../files/large.fld ../files/results/large-1000.fld
+
+
---
+++ scripts/perf100.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+
+. /etc/profile
+module avail
+module load gcc/12.1.0
+
+perf stat -r 5 ../build/fluid/fluid 100 ../files/large.fld ../files/results/large-1000.fld
